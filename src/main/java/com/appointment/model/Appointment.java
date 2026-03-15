863237c3e65dac418646ef77e3aeaaa968745f78
Chandresh-P-358 <717824p210@kce.ac.in>
2026-03-15 23:54:07 +0530

    Add files via upload

---
+++ src/main/java/com/appointment/model/Appointment.java
@@ -0,0 +1,21 @@
+package com.appointment.model; public class Appointment {
+private int appointmentId; private String clientName; private String service; private String date; private String time;
+public int getAppointmentId() { return appointmentId; }
+public void setAppointmentId(int appointmentId) { this.appointmentId = appointmentId;}
+public String getClientName() { return clientName; }
+public void setClientName(String clientName) { this.clientName = clientName;
+}
+public String getService() { return service;
+}
+ 
+public void setService(String service) { this.service = service;
+}
+public String getDate() { return date;
+}
+public void setDate(String date) { this.date = date;
+}
+
+
+public String getTime() { return time;} public void setTime(String time) {
+this.time = time;
+}}
